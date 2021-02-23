57d5736fe94565f46d7f573e17c544db887c974d
Seth R Johnson <johnsonsr@ornl.gov>
2021-02-23 12:28:20 -0500

    Revert "Remove grid striding"
    This reverts commit b73a69ddd625f369bc6130375860ef671da65e93.

--- app/demo-interactor/KNDemoKernel.cu
+++ app/demo-interactor/KNDemoKernel.cu
@@ -32,27 +32,32 @@ namespace
 //---------------------------------------------------------------------------//
 /*!
  * Kernel to initialize particle data.
+ *
+ * For testing purposes (this might not be the case for the final app) we use a
+ * grid-stride loop rather than requiring that each thread correspond exactly
+ * to a particle track. In other words, this method allows a single warp to
+ * operate on two 32-thread chunks of data.
+ *  https://developer.nvidia.com/blog/cuda-pro-tip-write-flexible-kernels-grid-stride-loops/
  */
 __global__ void initialize_kernel(ParamsDeviceRef const params,
                                   StateDeviceRef const  states,
                                   InitialPointers const init)
 {
-    unsigned int tid = blockIdx.x * blockDim.x + threadIdx.x;
-
-    // Exit if out of range or already dead
-    if (tid >= states.size())
+    // Grid-stride loop, see
+    for (int tid = blockIdx.x * blockDim.x + threadIdx.x;
+         tid < static_cast<int>(states.size());
+         tid += blockDim.x * gridDim.x)
     {
-        return;
+        ParticleTrackView particle(
+            params.particle, states.particle, ThreadId(tid));
+        particle = init.particle;
+
+        // Particles begin alive and in the +z direction
+        states.direction[tid] = {0, 0, 1};
+        states.position[tid]  = {0, 0, 0};
+        states.time[tid]      = 0;
+        states.alive[tid]     = true;
     }
-
-    ParticleTrackView particle(params.particle, states.particle, ThreadId(tid));
-    particle = init.particle;
-
-    // Particles begin alive and in the +z direction
-    states.direction[tid] = {0, 0, 1};
-    states.position[tid]  = {0, 0, 0};
-    states.time[tid]      = 0;
-    states.alive[tid]     = true;
 }
 
 //---------------------------------------------------------------------------//
@@ -75,66 +80,70 @@ __global__ void iterate_kernel(ParamsDeviceRef const            params,
     DetectorView           detector_hit(detector);
     PhysicsGridCalculator  calc_xs(params.tables.xs, params.tables.reals);
 
-    unsigned int tid = blockIdx.x * blockDim.x + threadIdx.x;
-
-    // Exit if out of range or already dead
-    if (tid >= states.size() || !states.alive[tid])
+    for (unsigned int tid = blockIdx.x * blockDim.x + threadIdx.x;
+         tid < states.size();
+         tid += blockDim.x * gridDim.x)
     {
-        return;
+        // Skip loop if already dead
+        if (!states.alive[tid])
+        {
+            continue;
+        }
+
+        // Construct particle accessor from immutable and thread-local data
+        ParticleTrackView particle(
+            params.particle, states.particle, ThreadId(tid));
+        RngEngine rng(states.rng, ThreadId(tid));
+
+        // Move to collision
+        demo_interactor::move_to_collision(particle,
+                                           calc_xs,
+                                           states.direction[tid],
+                                           &states.position[tid],
+                                           &states.time[tid],
+                                           rng);
+
+        Hit h;
+        h.pos    = states.position[tid];
+        h.thread = ThreadId(tid);
+        h.time   = states.time[tid];
+
+        if (particle.energy() < units::MevEnergy{0.01})
+        {
+            // Particle is below interaction energy
+            h.dir              = states.direction[tid];
+            h.energy_deposited = particle.energy();
+
+            // Deposit energy and kill
+            detector_hit(h);
+            states.alive[tid] = false;
+            continue;
+        }
+
+        // Construct RNG and interaction interfaces
+        KleinNishinaInteractor interact(params.kn_interactor,
+                                        particle,
+                                        states.direction[tid],
+                                        allocate_secondaries);
+
+        // Perform interaction: should emit a single particle (an electron)
+        Interaction interaction = interact(rng);
+        CELER_ASSERT(interaction);
+        CELER_ASSERT(interaction.secondaries.size() == 1);
+
+        // Deposit energy from the secondary (effectively, an infinite energy
+        // cutoff)
+        {
+            const auto& secondary = interaction.secondaries.front();
+            h.dir                 = secondary.direction;
+            h.energy_deposited    = secondary.energy;
+            detector_hit(h);
+        }
+
+        // Update post-interaction state (apply interaction)
+        states.direction[tid] = interaction.direction;
+        particle.energy(interaction.energy);
     }
-
-    // Construct particle accessor from immutable and thread-local data
-    ParticleTrackView particle(params.particle, states.particle, ThreadId(tid));
-    RngEngine         rng(states.rng, ThreadId(tid));
-
-    // Move to collision
-    demo_interactor::move_to_collision(particle,
-                                       calc_xs,
-                                       states.direction[tid],
-                                       &states.position[tid],
-                                       &states.time[tid],
-                                       rng);
-
-    Hit h;
-    h.pos    = states.position[tid];
-    h.thread = ThreadId(tid);
-    h.time   = states.time[tid];
-
-    if (particle.energy() < units::MevEnergy{0.01})
-    {
-        // Particle is below interaction energy
-        h.dir              = states.direction[tid];
-        h.energy_deposited = particle.energy();
-
-        // Deposit energy and kill
-        detector_hit(h);
-        states.alive[tid] = false;
-        return;
-    }
-
-    // Construct RNG and interaction interfaces
-    KleinNishinaInteractor interact(params.kn_interactor,
-                                    particle,
-                                    states.direction[tid],
-                                    allocate_secondaries);
-
-    // Perform interaction: should emit a single particle (an electron)
-    Interaction interaction = interact(rng);
-    CELER_ASSERT(interaction);
-    CELER_ASSERT(interaction.secondaries.size() == 1);
-
-    // Deposit energy from the secondary (effectively, an infinite energy
-    // cutoff)
-    {
-        const auto& secondary = interaction.secondaries.front();
-        h.dir                 = secondary.direction;
-        h.energy_deposited    = secondary.energy;
-        detector_hit(h);
-    }
-
-    // Update post-interaction state (apply interaction)
-    states.direction[tid] = interaction.direction;
-    particle.energy(interaction.energy);
 }
 } // namespace
 
@@ -144,14 +153,17 @@ __global__ void iterate_kernel(ParamsDeviceRef const            params,
 /*!
  * Initialize particle states.
  */
-void initialize(const CudaGridParams&  opts,
+void initialize(const CudaGridParams&  grid,
                 const ParamsDeviceRef& params,
                 const StateDeviceRef&  states,
                 const InitialPointers& initial)
 {
+    // TODO: remove grid params in favor of one thread per track. In the
+    // meantime, `calc_kernel_params` registers the kernel call and occupancy
+    // with the diagnostics.
     static const KernelParamCalculator calc_kernel_params(
-        initialize_kernel, "initialize", opts.block_size);
-    auto grid = calc_kernel_params(states.size());
+        initialize_kernel, "initialize", grid.block_size);
+    calc_kernel_params(states.size());
 
     CELER_EXPECT(states.alive.size() == states.size());
     CELER_EXPECT(states.rng.size() == states.size());
@@ -164,21 +176,22 @@ void initialize(const CudaGridParams&  opts,
 /*!
  * Run an iteration.
  */
-void iterate(const CudaGridParams&              opts,
+void iterate(const CudaGridParams&              grid,
              const ParamsDeviceRef&             params,
              const StateDeviceRef&              states,
              const SecondaryAllocatorPointers&  secondaries,
              const celeritas::DetectorPointers& detector)
 {
+    // TODO: remove grid params, see above
     static const KernelParamCalculator calc_kernel_params(
-        iterate_kernel, "iterate", opts.block_size);
-    auto grid = calc_kernel_params(states.size());
+        iterate_kernel, "iterate", grid.block_size);
+    calc_kernel_params(states.size());
 
     iterate_kernel<<<grid.grid_size, grid.block_size>>>(
         params, states, secondaries, detector);
     CELER_CUDA_CHECK_ERROR();
 
-    if (opts.sync)
+    if (grid.sync)
     {
         // Note: the device synchronize is useful for debugging and necessary
         // for timing diagnostics.
