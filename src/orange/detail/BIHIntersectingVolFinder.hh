2d3a614c5003a5e033cb3dda9157eff80d0e30f9
Seth R Johnson <johnsonsr@ornl.gov>
2026-05-01 06:48:29 -0400

    Confirm bbox test makes it slower :'(

--- src/orange/detail/BIHIntersectingVolFinder.hh
+++ src/orange/detail/BIHIntersectingVolFinder.hh
@@ -135,11 +135,6 @@ BIHIntersectingVolFinder::operator()(BIHIntersectingVolFinder::Ray ray,
     while (!stack.empty())
     {
         BIHNodeId current_node = stack.pop();
-        if (!this->hits_bbox(view_.bbox(current_node), ray, max_search_dist))
-        {
-            // No intersection with this node/leaf
-            continue;
-        }
         if (!view_.is_inner(current_node))
         {
             intersection = this->visit_leaf(
