6e42a98e83f3ca98da7b4f50c1514405414916b2
Seth R Johnson <johnsonsr@ornl.gov>
2026-09-07 08:24:20 -0400

    Try getting base sha from remote and fetching

--- scripts/ci/clang-tidy-changed.sh
+++ scripts/ci/clang-tidy-changed.sh
@@ -4,15 +4,18 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 #-----------------------------------------------------------------------------#
 
-SOURCE_DIR="$PWD"
-BUILD_DIR="$PWD/build"
-BASE_REF="$1"
-
 log() {
   printf "%s: %s\n" "$1" "$2" >&2
 }
-if [ -z "${BASE_REF}" ]; then
-  log error "base ref not defined: pass as first argument"
+
+SOURCE_DIR="$PWD"
+BUILD_DIR="$PWD/build"
+REMOTE="$1"
+BASE_SHA="$2"
+HEAD_SHA="HEAD"
+
+if [ $# -ne 2 ]; then
+  log usage "CLANG_TIDY=path $0 remote base_sha"
   exit 1
 fi
 
@@ -21,9 +24,10 @@ if [ -z "$CLANG_TIDY" ]; then
   exit 1
 fi
 
-log info "Finding changes from ${BASE_REF}"
-BASE=$(git merge-base "${BASE_REF}" HEAD)
-ALL_FILES=$(git diff --name-only --diff-filter=ACM "$BASE" HEAD)
+log info "Fetching base commit ${BASE_SHA} from ${REMOTE}"
+git fetch --depth 1 "${REMOTE}" "${BASE_SHA}"
+
+ALL_FILES=$(git diff --name-only --diff-filter=ACM "$BASE_SHA" "$HEAD_SHA")
 set +e
 CC_FILES=$(grep -E '^(src|app)/.*\.cc$' - <<< "$ALL_FILES")
 
@@ -43,4 +47,3 @@ if [ -z "$CC_FILES" ]; then
 fi
 log info "Running clang-tidy on: $CC_FILES"
 $CLANG_TIDY -p $BUILD_DIR $CC_FILES
-exit $?
